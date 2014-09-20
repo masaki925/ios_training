5f71a35613d2078360569ca34f1231a83526206f
masaki925 <masaki925@gmail.com>
2014-09-20 16:31:36 +0900

    add TableViewController

---
+++ SQLiteTest01/SQLiteTest01/SQLiteTest01/FailedBanksListViewController.h
@@ -0,0 +1,15 @@
+//
+//  FailedBanksListViewController.h
+//  SQLiteTest01
+//
+//  Created by Masaki Iwamoto on 2014/09/20.
+//  Copyright (c) 2014年 Wanderlust, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FailedBanksListViewController : UITableViewController
+
+@property (nonatomic, retain) NSArray *failedBankInfos;
+
+@end
